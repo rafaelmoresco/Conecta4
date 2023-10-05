fe19ff6ded301fffd790c22d69dd5e95f560b266
Moresco <rafaelmorescovieira@gmail.com>
2023-10-05 18:19:04 -0300

    change ai

--- conecta4.py
+++ conecta4.py
@@ -10,25 +10,6 @@ vertical_kernel = np.transpose(horizontal_kernel)
 diag1_kernel = np.eye(4, dtype=np.uint8)
 diag2_kernel = np.fliplr(diag1_kernel)
 detection_kernels = [horizontal_kernel, vertical_kernel, diag1_kernel, diag2_kernel]
-"""
-class GameOfBones( TwoPlayerGame ):
-     In turn, the players remove one, two or three bones from a
-    pile of bones. The player who removes the last bone loses. 
-
-    def __init__(self, players=None):
-        self.players = players
-        self.pile = 20 # start with 20 bones in the pile
-        self.current_player = 1 # player 1 starts
-
-    def possible_moves(self): return ['1','2','3']
-    def make_move(self,move): self.pile -= int(move) # remove bones.
-    def win(self): return self.pile<=0 # opponent took the last bone ?
-    def is_over(self): return self.win() # Game stops when someone wins.
-    def show(self): print ("%d bones left in the pile" % self.pile)
-    def scoring(self): return 100 if game.win() else 0 # For the AI
-"""
-# Start a match (and store the history of moves when it ends)
-
 
 class Conecta4(TwoPlayerGame):
     def __init__(self, players=None) -> None:
@@ -42,7 +23,11 @@ class Conecta4(TwoPlayerGame):
         self.p2Board = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
 
     def possible_moves(self): 
-        return['0','1','2','3','4','5']
+        returnValue = []
+        for i in range:
+            if self.boardAux[i] < 7:
+                returnValue.append(str(self.boardAux[i]))
+        return returnValue
     def make_move(self,move): 
         self.board[int(move)][6-self.boardAux[int(move)]] = self.current_player
         if self.current_player == 1:    
@@ -67,7 +52,13 @@ class Conecta4(TwoPlayerGame):
     def show(self): 
         pprint (self.board)
     def scoring(self): 
-        return 100 if game.win() else 0 # For the AI
+        if game.win():
+            return 100
+        elif game.is_over():
+            return 50
+        else:
+            return 0
+        
 
 ai = Negamax(100) # The AI will think 13 moves in advance
 game = Conecta4( [ Human_Player(), AI_Player(ai) ] )
